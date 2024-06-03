5d4cdc5077bed020c00df9313de37894436863ff
Mono The Protogen <64149624+MonoTheProtogen@users.noreply.github.com>
2024-06-03 14:32:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
@@ -29,6 +30,8 @@ if(isset($_POST['wyszukaj']))
 }
 ?>
 
+
+<script src="./script.js"></script>
 
 </body>
 </html>
