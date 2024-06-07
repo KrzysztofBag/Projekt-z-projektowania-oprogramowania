090f5976d14bbab0ef3f755c32998b2acd7d5f06
KrzysztofBag <baginski.krzysztof@zs6sobieski.edu.pl>
2024-06-07 15:58:55 +0200

    Dodanie wyszukiwania po tytule i autorze

--- index.php
+++ index.php
@@ -4,12 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Biblioteka Muzyki</title>
+    <script src="./script.js" defer></script>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
 <div id="audiodiv">
 
+</div>
+
+<div id="obsluga">
+    <form action="./index.php" method="post">
+        <span>Po czym chcesz wyszukać</span>
+        <br>
+        <label for="tytul">Tytuł<input type="radio" name="tytul_autor" class='tytul_autor' id="tytul" value="nazwa"></label>
+        <label for="autor">Autor<input type="radio" name="tytul_autor" class='tytul_autor' id="autor" value="Autor"></label>
+        <input type="text" name="text" id="wyszukaj" style="display:none;">
+        <br>
+        <input type="submit" value="Szukaj" name="szukaj">
+    </form>
 </div>
     
 <?php
@@ -18,9 +31,18 @@ require "dbconnect.php";
 $con = mysqli_connect($host,$user,$password);
 mysqli_select_db($con,$db);
 
-if(isset($_POST['wyszukaj']))
+if(isset($_POST['szukaj']))
 {
-
+    $wybor = $_POST['tytul_autor'];
+    $text = $_POST['text'];
+    $q = mysqli_query($con,"SELECT * FROM `biblioteka` WHERE biblioteka.$wybor LIKE '$text %'");
+    while($row=mysqli_fetch_array($q))
+    {
+        echo "<div>";
+        echo "<span>$row[1]</span>";
+        echo "<input type='button' value='Wybierz' data-link='$row[2]' class='wybor' />";
+        echo "</div>";
+    }
 }else
 {
     $q = mysqli_query($con,"SELECT * from biblioteka");
@@ -33,10 +55,6 @@ if(isset($_POST['wyszukaj']))
     }
 }
 ?>
-
-
-
-
 
 </body>
 </html>
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const wybor = document.querySelectorAll(".wybor")
 const audiodiv = document.getElementById("audiodiv")
+const tytul_autor = document.querySelectorAll(".tytul_autor")
+const wyszukaj = document.getElementById("wyszukaj")
 
 for(let w of wybor)
 {
@@ -8,3 +10,11 @@ for(let w of wybor)
         audiodiv.innerHTML = w.dataset.link
     })
 }
+
+for(let ta of tytul_autor)
+    {
+        ta.addEventListener("input", function()
+    {
+        wyszukaj.style.display="block"
+    })
+    }
